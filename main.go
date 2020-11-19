a4d45b13fe8405bd44afef1c601bedaaca1c6838
Sean Dever <seand0611@gmail.com>
2020-11-19 08:16:53 +0000

    Refactored solution

--- main.go
+++ main.go
@@ -22,32 +22,24 @@ func validAnagram(s1 string, s2 string) bool {
 	//Split both strings
 	firstSplit := strings.Split(s1, "")
 	secondSplit := strings.Split(s2, "")
-	firstStringMap := make(map[string]int)
-	secondStringMap := make(map[string]int)
-	//Iterate through each letter of the strings and place them in a map with the key being the char and value being number of occurances
+	lettersMap := make(map[string]int)
+
+	//Iterate through each letter of the first string and place them in a map with the key being the char and value being number of occurances
 	for _, i := range firstSplit {
-		_, ok := firstStringMap[i]
+		_, ok := lettersMap[i]
 		if ok {
-			firstStringMap[i] += 1
+			lettersMap[i] += 1
 		} else {
-			firstStringMap[i] = 1
+			lettersMap[i] = 1
 		}
 	}
+  //Iterate through the second string and lookup to see if the lettersMap contains all the the letters within the second string
 	for _, i := range secondSplit {
-    _, ok := secondStringMap[i]
-		if ok {
-			secondStringMap[i] += 1
-		} else {
-			secondStringMap[i] = 1
-		}
-	}
-	//If the number of occurences for each letter do not match it is not an anagram
-	for key, _ := range firstStringMap {
-		_, ok := secondStringMap[key]
-		if !ok {
-			fmt.Println("false")
-			return false
-		}
+    //If there are any that do not exist within lettersMap, return false
+    if _, ok := lettersMap[i]; !ok {
+      fmt.Println("false")
+      return false
+    }
 	}
 	fmt.Println("true")
 	return true
@@ -55,5 +47,5 @@ func validAnagram(s1 string, s2 string) bool {
 
 
 func main() {
-	validAnagram("rat", "tar")
+	validAnagram("rat", "gar")
 }
