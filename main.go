0314f3d79429c928bc07a743994f71668ba47dbb
Sean Dever <seand0611@gmail.com>
2020-11-29 02:59:17 +0000

    refactored for readability

--- main.go
+++ main.go
@@ -5,26 +5,19 @@ import (
 	"strings"
 )
 
-/*
-Given two strings, write a fucntion to determine if the second string is an anagram of the first.
-
-ex:
-validAnagram("awesome", "awesom") //false
-validAnagram("anagram", "nagaram") //true
-*/
-
 func validAnagram(s1 string, s2 string) bool {
 	//Place a check to see if lengths of each string are the same
 	if len(s1) != len(s2) {
-		fmt.Println("false")
 		return false
 	}
-	//Split both strings
+	//Split both of the strings, so we can get an array of letters that composes the string.
 	firstSplit := strings.Split(s1, "")
 	secondSplit := strings.Split(s2, "")
+
+	//Initiate an empty map that we'll use to store our characters in the first array.
 	lettersMap := make(map[string]int)
 
-	//Iterate through each letter of the first string and place them in a map with the key being the char and value being number of occurances
+	//Iterate through each letter of the first string and place them in the map with the key being the character and value being the number of occurrences
 	for _, i := range firstSplit {
 		_, ok := lettersMap[i]
 		if ok {
@@ -33,21 +26,22 @@ func validAnagram(s1 string, s2 string) bool {
 			lettersMap[i] = 1
 		}
 	}
-  //Iterate through the second string and lookup to see if the lettersMap contains all the the letters within the second string
+	//Iterate through the second string and lookup to see if the map contains all the letters within the second string.
 	for _, i := range secondSplit {
-    //If there are any that do not exist within lettersMap, return false
-    if lettersMap[i] <= 0{
-      fmt.Println("false")
-      return false
-    } else {
-      lettersMap[i] -= 1
-    }
+		//If there are any that do not exist within the map, return false.
+		if lettersMap[i] <= 0 {
+			return false
+		} else {
+			//Because the count of that letter is greater than 0, we're gonna reduce the count of that element by 1.
+			lettersMap[i] -= 1
+		}
 	}
-	fmt.Println("true")
+	//return true because the number of occurrences for each letter within the second string matches up with the ones in the first string.
 	return true
 }
 
-
 func main() {
-	validAnagram("rat", "rar")
+	fmt.Println(validAnagram("helloworld", "oowldlrhle")) //true
+	fmt.Println(validAnagram("rat", "ta"))                //false
+	fmt.Println(validAnagram("rat", "cat"))               //false
 }
